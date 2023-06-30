317139c4b658745b2932536dd1f830f6fb07b4dc
hackerswat <ha>
2023-06-30 05:08:46 -0700

    Disable sample submission option added

--- Defender.bat
+++ Defender.bat
@@ -12,10 +12,13 @@ if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 echo Choose an option:
 echo 1. Disable Windows Defender
 echo 2. Enable Windows Defender
+echo 3. Disable sample submission
+echo 4. Enable sample submission
 set /p option="Enter option number: "
 
 if "%option%"=="1" (
     echo Disabling Windows Defender...
+    
     Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "1" /f
     Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "1" /f
     Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "1" /f
@@ -31,6 +34,7 @@ if "%option%"=="1" (
 
 if "%option%"=="2" (
     echo Enabling Windows Defender...
+    
     Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t REG_DWORD /d "0" /f
     Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t REG_DWORD /d "0" /f
     Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableOnAccessProtection" /t REG_DWORD /d "0" /f
@@ -39,6 +43,28 @@ if "%option%"=="2" (
     Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments" /v "ScanWithAntiVirus" /t REG_DWORD /d "3" /f
 
     echo Windows Defender has been enabled.
+    echo Your settings have changed. Please reboot your computer for these changes to take effect!
+    pause
+    exit
+)
+
+if "%option%"=="3" (
+    echo Disabling sample submission...
+    
+    Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SubmitSamplesConsent" /t REG_DWORD /d "2" /f
+
+    echo sample submission has been Disabled.
+    echo Your settings have changed. Please reboot your computer for these changes to take effect!
+    pause
+    exit
+)
+
+if "%option%"=="4" (
+    echo Enabling sample submission...
+    
+    echo yes | Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" /v "SubmitSamplesConsent"
+    
+    echo sample submission has been enabled.
     echo Your settings have changed. Please reboot your computer for these changes to take effect!
     pause
     exit
